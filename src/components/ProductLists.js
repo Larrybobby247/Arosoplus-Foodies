70c50fb7baecd1c8da5bb497333ac74e02502999
Lawrence Adebowale <larrybobby247@gmail.com>
2026-04-13 22:32:35 +0100

    second commit

--- src/components/ProductLists.js
+++ src/components/ProductLists.js
@@ -3,24 +3,28 @@ import Product1 from '../assets/product1.jpg';
 import Product2 from '../assets/product2.jpg';
 import Product3 from '../assets/product3.jpg';
 import Product4 from '../assets/product4.jpg';
-// import Product5 from '../assets/Product5.jpg';
+import Product5 from '../assets/Product5.jpg';
 import Product6 from '../assets/product6.jpg';
+import Product7 from '../assets/product7.jpg';
+import Product8 from '../assets/product8.jpg';
+import Product9 from '../assets/product9.jpg';
+import Product10 from '../assets/product10.jpg';
 
 export const ProductLists = [
   {
     id: 1,
-    name: "Smokey Party Jollof Rice",
-    description: "Our signature firewood-flavored Jollof, served with peppered proteins and sides.",
+    name: "Smokey Jollof with plantain and turkey",
+    // description: "Our signature firewood-flavored Jollof, served with peppered proteins and sides.",
     arrival: "top", // Best seller
     image: Product1,
     category: "Bulk Meals",
-    minOrder: "10 Plates",
+    minOrder: "2 Plates",
     priceTag: "Premium" 
   },
   {
     id: 2,
-    name: "Native Fisherman Soup",
-    description: "Rich, authentic riverine soup loaded with fresh seafood, snails, and dried fish.",
+    name: "3 in 1 package: Jollof, fried rice and spagetti combo",
+    // description: "Rich, authentic riverine soup loaded with fresh seafood, snails, and dried fish.",
     arrival: "regular",
     image: Product2,
     category: "Soup Bowls",
@@ -29,8 +33,8 @@ export const ProductLists = [
   },
   {
     id: 3,
-    name: "Efo Riro (Lagos Style)",
-    description: "Rich spinach soup prepared with palm oil, locust beans, and assorted obstacles.",
+    name: "Food Package combo",
+    // description: "Rich spinach soup prepared with palm oil, locust beans, and assorted obstacles.",
     arrival: "top",
     image: Product3,
     category: "Soup Bowls",
@@ -39,29 +43,65 @@ export const ProductLists = [
   },
   {
     id: 4,
-    name: "Arosoplus Luxury Food Tray",
-    description: "A curated celebration tray featuring variety: pasta, proteins, snacks, and fruits.",
+    name: "Luxery Rice with fried fish with plantaion and salad",
+    // description: "A curated celebration tray featuring variety: pasta, proteins, snacks, and fruits.",
     arrival: "top", 
     image: Product4,
     category: "Trays & Gifts",
     customizable: true,
     priceTag: "Exclusive"
   },
-  // {
-  //   id: 5,
-  //   name: "Abula Special (Amala/Gbegiri/Ewedu)",
-  //   description: "The classic Ibadan favorite. Soft Amala served with silky Gbegiri and Ewedu soup.",
-  //   arrival: "regular",
-  //   image: Product5,
-  //   category: "Event Specials",
-  //   priceTag: "Premium"
-  // },
+  {
+    id: 5,
+    name: "Tasty Small chops",
+    // description: "The classic Ibadan favorite. Soft Amala served with silky Gbegiri and Ewedu soup.",
+    arrival: "regular",
+    image: Product5,
+    category: "Event Specials",
+    priceTag: "Premium"
+  },
   {
     id: 6,
-    name: "Peppered Turkey & Snail Platter",
-    description: "Spiced, sautéed turkey and snails garnished with onions and bell peppers.",
+    name: "Macaroni dessert",
+    // description: "Spiced, sautéed turkey and snails garnished with onions and bell peppers.",
     arrival: "regular",
     image: Product6,
+    category: "Sides & Proteins",
+    priceTag: "Premium"
+  },
+  {
+    id: 7,
+    name: "Yellow garri served with ogbonor",
+    // description: "Spiced, sautéed turkey and snails garnished with onions and bell peppers.",
+    arrival: "top",
+    image: Product7,
+    category: "Sides & Proteins",
+    priceTag: "Premium"
+  },
+  {
+    id: 8,
+    name: "Spicy spagetti with meat",
+    // description: "Spiced, sautéed turkey and snails garnished with onions and bell peppers.",
+    arrival: "top",
+    image: Product8,
+    category: "Sides & Proteins",
+    priceTag: "Premium"
+  },
+  {
+    id: 9,
+    name: "Burger",
+    // description: "Spiced, sautéed turkey and snails garnished with onions and bell peppers.",
+    arrival: "regular",
+    image: Product9,
+    category: "Sides & Proteins",
+    priceTag: "Premium"
+  },
+  {
+    id: 10,
+    name: "Canapes Setting",
+    // description: "Spiced, sautéed turkey and snails garnished with onions and bell peppers.",
+    arrival: "top",
+    image: Product10,
     category: "Sides & Proteins",
     priceTag: "Premium"
   }
